6cdba6c8a51cd1eb5a11434cfe4e9bc68f1cc654
Thomas <thomas.soret@orange.fr>
2026-07-13 16:51:10 +0200

    feat: authentification register et login avec JWT et Argon2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,14 @@
 import "dotenv/config";
 import pool from './config/db.js';
 import express from "express";
+import authRoutes from './routes/authRoutes.js';
+
 
 const app = express();
 
 app.use(express.json())
+app.use('/api/auth', authRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 
