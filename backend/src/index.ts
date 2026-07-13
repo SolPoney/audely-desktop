25d1d4d33589a1c48bcb40c4f5caa436be0101a3
Thomas <thomas.soret@orange.fr>
2026-07-13 15:03:03 +0200

    chore: initialisation backend Express TypeScript ESM

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Serveur lancé sur ${PORT}`))
