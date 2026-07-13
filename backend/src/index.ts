20ba07d2ddadd171a21356c62851c76ee41807eb
Thomas <thomas.soret@orange.fr>
2026-07-13 17:52:30 +0200

    feat: add exercises and categories routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,15 +2,19 @@ import 'dotenv/config';
 import express from 'express';
 import pool from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
+import exerciceRoutes from './routes/exerciceRoutes.js';
+
 import { verifyToken } from './middlewares/authMiddleware.js';
 
 const app = express();
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api', exerciceRoutes);
 
 app.get('/api/protected', verifyToken, (req, res) => {
   res.json({ message: 'Accès autorisé', user: (req as any).user });
 });
+
 
 const PORT = process.env.PORT || 3000;
 
